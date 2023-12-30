5ac301200bdfb2afa2651f3224c5d4b8b84d7052
Mzzqq <rezarifa661@gmail.com>
2023-12-30 14:13:14 +0700

    fix(main): Correct import block placement in main function

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
+	"server/db"
+)
 
+func main() {
+	_, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("could not initialize database connection: %s", err)
+	}
 }
