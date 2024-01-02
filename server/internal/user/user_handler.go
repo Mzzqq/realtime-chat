0d49a9481075390d770a02e869351d6c731dd1df
Mzzqq <rezarifa661@gmail.com>
2024-01-02 22:10:46 +0700

    feat(user_handler): Extend CreateUser handle
    Extend CreateUser handler to interact with Service layer

--- server/internal/user/user_handler.go
+++ server/internal/user/user_handler.go
@@ -19,5 +19,14 @@ func (h *Handler) CreateUser(c *gin.Context) {
 	var u CreateUserReq
 	if err := c.ShouldBindJSON(&u); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+
+	res, err := h.Service.CreateUser(c.Request.Context(), &u)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
 }
