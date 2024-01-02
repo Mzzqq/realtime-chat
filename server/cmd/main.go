f6e60e26a8a12c094c73cd1b6573e38c5ff1c6ba
Mzzqq <rezarifa661@gmail.com>
2024-01-02 22:14:17 +0700

    feat(main): Update main func
    Update main function for initializing database, user repository, service, and handler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("could not initialize database connection: %s", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
 }
