0e2eb3491bdd2114151993feebe2768d13878153
Mzzqq <rezarifa661@gmail.com>
2024-01-03 21:59:43 +0700

    feat(router): Initialize signup endpoint

--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"server/internal/user"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler) {
+	r = gin.Default()
+
+	r.POST("/signup", userHandler.CreateUser)
+}
