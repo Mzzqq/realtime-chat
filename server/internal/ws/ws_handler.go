2277f96fbc88ef80b7821d5e3cc1bc5747752956
Mzzqq <rezarifa661@gmail.com>
2024-01-04 22:36:24 +0700

    feat(ws_handler): Add CreateRoomReq struct

--- server/internal/ws/ws_handler.go
+++ server/internal/ws/ws_handler.go
@@ -4,6 +4,11 @@ type Handler struct {
 	hub *Hub
 }
 
+type CreateRoomReq struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 func NewHandler(h *Hub) *Handler {
 	return &Handler{
 		hub: h,
