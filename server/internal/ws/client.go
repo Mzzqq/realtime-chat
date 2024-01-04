23771d36f2f9ce80376dd76ca5a1d37239940b93
Mzzqq <rezarifa661@gmail.com>
2024-01-04 22:08:17 +0700

    feat(client): Add Client struct

--- server/internal/ws/client.go
+++ server/internal/ws/client.go
@@ -1,5 +1,12 @@
 package ws
 
+import "golang.org/x/net/websocket"
+
+type Client struct {
+	Conn    *websocket.Conn
+	Message chan *Message
+}
+
 type Message struct {
 	Content  string `json:"content"`
 	RoomID   string `json:"roomId"`
