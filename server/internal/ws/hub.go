02e76589cc334ddb7f756c7f920bf90e54067810
Mzzqq <rezarifa661@gmail.com>
2024-01-04 22:16:34 +0700

    feat(hub): Add Room & Hub struct

--- server/internal/ws/hub.go
+++ server/internal/ws/hub.go
@@ -1,1 +1,11 @@
 package ws
+
+type Room struct {
+	ID      string             `json:"ID"`
+	Name    string             `json:"name"`
+	Clients map[string]*Client `json:"clients"`
+}
+
+type Hub struct {
+	Room map[string]*Room
+}
