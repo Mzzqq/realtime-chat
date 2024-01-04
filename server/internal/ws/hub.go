51b626e33815b9182e5d02e7af15d09f6228db49
Mzzqq <rezarifa661@gmail.com>
2024-01-04 23:39:00 +0700

    feat(hub): Implement Run() for ws Hub
    Added the `Run` method to the ws `Hub` struct, enabling the continuous management of WebSocket events. This method contains a loop with a select statement, handling client registration, unregistration, and message broadcasting within ws rooms. The `Run` method effectively manages ws interactions, ensuring seamless communication among clients.

--- server/internal/ws/hub.go
+++ server/internal/ws/hub.go
@@ -21,3 +21,40 @@ func NewHub() *Hub {
 		Broadcast:  make(chan *Message, 5),
 	}
 }
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				r := h.Rooms[cl.RoomID]
+
+				if _, ok := r.Clients[cl.ID]; !ok {
+					r.Clients[cl.ID] = cl
+				}
+			}
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				if _, ok := h.Rooms[cl.RoomID].Clients[cl.ID]; ok {
+					// Broadcast a message saying that the client has left the room
+					if len(h.Rooms[cl.RoomID].Clients) != 0 {
+						h.Broadcast <- &Message{
+							Content: "User left the chat",
+							RoomID:  cl.RoomID,
+							Username: cl.Username
+						}
+					}
+
+					delete(h.Rooms[cl.RoomID].Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
+		case m := <-h.Broadcast:
+			if _, ok := h.Rooms[m.RoomID]; ok {
+				for _, cl := range h.Rooms[m.RoomID].Clients {
+					cl.Message <- m
+				}
+			}
+		}
+	}
+}
