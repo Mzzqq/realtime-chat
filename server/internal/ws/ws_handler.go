d445e0641f667dc9401c11b48fa14737fe87da6e
Mzzqq <rezarifa661@gmail.com>
2024-01-05 23:02:36 +0700

    feat(ws_handler): define RoomRes struct

--- server/internal/ws/ws_handler.go
+++ server/internal/ws/ws_handler.go
@@ -86,6 +86,11 @@ func (h *Handler) JoinRoom(c *gin.Context) {
 	cl.readMessage(h.hub)
 }
 
+type RoomRes struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 func (h *Handler) GetRooms(c *gin.Context) {
-	
+
 }
