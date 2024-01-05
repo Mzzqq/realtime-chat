e68247851f73736f6e197bdbb48385b706eea39e
Mzzqq <rezarifa661@gmail.com>
2024-01-05 23:28:46 +0700

    fix(client): fix log

--- server/internal/ws/client.go
+++ server/internal/ws/client.go
@@ -44,7 +44,7 @@ func (c *Client) readMessage(hub *Hub) {
 		_, m, err := c.Conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Print("error: %v", err)
+				log.Printf("error: %v", err)
 			}
 			break
 		}
