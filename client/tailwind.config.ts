279d7f92dd8fb62d5336e17b854df877f63e7a4c
Mzzqq <rezarifa661@gmail.com>
2024-01-23 15:42:14 +0700

    style(client): Update Tailwind CSS color palette in tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'dark-primary': '#131a1c',
+      'dark-secondary': '#1b2224',
+      red: "#e74c4c",
+      green: "#6bb05d",
+      blue: "#0183ff",
+      grey: "#dddfe2",
+      white: "#fff",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
